cd85bd2a0299f9d08ade17ace9e9de3f29979c37
unknown <adampjessie@gmail.com>
2025-04-22 14:52:46 -0400

    Added constructors, getter/setter and checkIn/checkOut methods

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,84 @@
 package com.pluralsight;
 
 public class Book {
+
+    private int id;
+    private String isbn, title, checkedOutTo;
+    private boolean isCheckedOut;
+
+    public Book(int id, String isbn, String title, String checkedOutTo, boolean isCheckedOut) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.checkedOutTo = checkedOutTo;
+        this.isCheckedOut = isCheckedOut;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    @Override
+    public String toString() {
+        if (!isCheckedOut) {
+            return String.format("""
+                ID: %d  ISBN: %s  Title: %s
+                """, id, isbn, title);
+        } else {
+            return String.format("""
+                ID: %d  ISBN: %s  Title: %s  Current Holder: %s
+                """, id, isbn, title, checkedOutTo);
+        }
+    }
+
+    public void checkOut(String name) {
+        this.checkedOutTo = name;
+        this.isCheckedOut = true;
+
+
+    }
+
+    public void checkIn() {
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+
+    }
+
 }
+
